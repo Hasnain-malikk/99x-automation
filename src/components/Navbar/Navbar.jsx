fd87f481c45509204d4a43f851c84e01efeb7ea3
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2025-07-10 17:20:10 +0500

    router working perfectly

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Navbar.css'
+import navIcon from '/ReactJs/99x-automation/src/assets/nav-icon.png'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      this is nav
+      <div className="nav">
+        <div className="logo"><img src={navIcon} alt="" /></div>
+        <div className="navLinks">
+            <Link to = "/" >HOME</Link>
+            <Link to = "/aboutus" >ABOUT US</Link>
+            <Link to = "/services" >SERVICES</Link>
+            <Link to = "/faq" >FAQ</Link>
+        </div>
+      </div>
+      <div className="btn">this is button</div>
     </div>
   )
 }

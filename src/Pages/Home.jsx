bd5f9b0b325b66e0e248ec3f389fef00b730c097
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2025-07-11 12:43:04 +0500

    section1 added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,8 @@ const Home = () => {
   return (
     <div className='home'>
       <Header/>
+      <div className='heading2' style={{marginTop:"-65px"}} >FOR REAL</div>
+      <div className='heading2'>BUSINESS GROWTH</div>
     </div>
   )
 }

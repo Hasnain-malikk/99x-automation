f2e2b3f5880d109bc95d76186325966e59f15ee8
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2025-07-12 15:04:19 +0500

    cards modified

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Card.css'
 import mic from '/ReactJs/99x-automation/src/assets/mic.png'
+import line from '/ReactJs/99x-automation/src/assets/line.png'
 
 const Card = () => {
   return (
     <div className='cards'>
       <img style={{height:"40px",margin:"11px 0"}} src={mic} alt="" />
       <h3>SPA Booking AI</h3>
+      <img style={{height:"8px",margin:"8px 170px 0 0"}} src={line} alt="" />
+      <div style={{margin: "10px 110px 0 0"}} className="para">Effortless spa</div>
+      <div style={{marginRight:"109px"}} className="para">appointment</div>
+      <div style={{marginRight:"95px"}} className="para">personalized se</div>
+      <div style={{marginRight:"62px"}} className="para">personalized service</div>
+      <div style={{marginRight:"68px"}} className="para">recommendations.</div>
+      <button className='card-btn' >Start Conversation</button>
     </div>
   )
 }
